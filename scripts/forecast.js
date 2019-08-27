373552523c8e2281654b34b8373a4a77589450dd
Terry Mafura <maffsojah1@gmail.com>
2019-08-27 12:49:12 +0200

    new weather app

---
+++ scripts/forecast.js
@@ -0,0 +1,25 @@
+const key = 'ZbS2t9mZv9AafPD638gp7rQcO6NvSvZ6';
+
+// get weather info
+const getWeather = async (id) => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// get city info
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
