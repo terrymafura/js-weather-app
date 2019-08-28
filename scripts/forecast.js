02e5d50c32dbb6760939a06279334939d3e909aa
Terry Mafura <maffsojah1@gmail.com>
2019-08-28 09:01:21 +0200

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -3,7 +3,7 @@ const key = 'ZbS2t9mZv9AafPD638gp7rQcO6NvSvZ6';
 // get weather info
 const getWeather = async (id) => {
 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
     const query = `${id}?apikey=${key}`;
 
     const response = await fetch(base + query);
@@ -15,7 +15,7 @@ const getWeather = async (id) => {
 // get city info
 const getCity = async (city) => {
 
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${key}&q=${city}`;
 
     const response = await fetch(base + query);
