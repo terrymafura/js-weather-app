480c44aee597b1c9f7086d622497986cfeef422f
Terry Mafura <maffsojah1@gmail.com>
2020-03-12 08:40:51 +0200

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,8 @@
 class Forecast{
     constructor(){
         this.key = 'ZbS2t9mZv9AafPD638gp7rQcO6NvSvZ6';
-        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
-        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+        this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     }
     async updateCity(city){
         const cityDets = await this.getCity(city);
